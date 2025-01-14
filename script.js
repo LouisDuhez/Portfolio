84d9802869ea2f8ca8cfa173a879914be5a59149
LouisDuhez <158026663+LouisDuhez@users.noreply.github.com>
2025-01-14 15:05:27 +0100

    Video And project

--- script.js
+++ script.js
@@ -60,74 +60,96 @@ navLinks.forEach(link => {
   })
 });
 
-containerSkills = document.querySelector('.skills-container')
+const devContainer = document.querySelector('.dev-container');
 
-const skillsData = [
-  {
-      "id": 1,
-      "title": "Créativité",
-      "image_class": "crea",
-      "image_name": "menu-ps",  // Nom de la première image
-      "image_name2": "photoshopcolor", // Nom de la deuxième image
-      "image_name3": "photoshop-toolBar",  // Nom de la troisième image
-      "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae id optio quas, reprehenderit ipsum quos qui pariatur maiores sunt maxime.",
-      "format" : "jpg"
-  },
-  {
-      "id": 2,
-      "title": "Développement Web",
-      "image_class": "dev",
-      "image_name": "HTML5",  // Nom de la première image
-      "image_name2": "CSS3", // Nom de la deuxième image
-      "image_name3": "Javascript",  // Nom de la troisième image
-      "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac urna nisi. Proin at neque et dui ultricies tincidunt.",
-      "format" :"svg"
-  },
-  {
-      "id": 3,
-      "title": "3D",
-      "image_class": "unreal",
-      "image_name": "firstImg3D",  // Nom de la première image
-      "image_name2": "secondImg3D", // Nom de la deuxième image
-      "image_name3": "thirdImg3D",  // Nom de la troisième image
-      "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod justo vitae neque fringilla, ac pretium ligula condimentum.",
-      "format" : "jpg"
-  },
-  {
-      "id": 4,
-      "title": "Montage vidéo",
-      "image_class": "video",
-      "image_name": "firstImgVideo",  // Nom de la première image
-      "image_name2": "secondImgVideo", // Nom de la deuxième image
-      "image_name3": "thirdImgVideo",  // Nom de la troisième image
-      "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet quos eveniet cupiditate tempora. Suscipit rerum commodi distinctio molestias.",
-      "format" : "jpg"
-  }
-];
 
-const lengthData = skillsData.length; 
-for (let i = 0; i < lengthData; i++) {
-  const skill = skillsData[i];  // récupère l'élément actuel
 
-  // Construction des chemins des images avec les noms dynamiques
-  const imagePath1 = `img/${skill.image_name}.${skill.format}`;
-  const imagePath2 = `img/${skill.image_name2}.${skill.format}`;
-  const imagePath3 = `img/${skill.image_name3}.${skill.format}`;
-
-  const cardHTML = `
-      <div class="skills-card">
-          <div class="skills-card-background ${skill.image_class}">
-          <img src="${imagePath1}" alt="" class="${skill.image_name}">
-              <img src="${imagePath2}" alt="" class="${skill.image_name2}">
-              <img src="${imagePath3}" alt="" class="${skill.image_name3}">
-          </div>
-          <div class="skills-card-text">
-              ${skill.text}
-          </div>
-              
-      </div>
-  `;
-
-  // Ajoute la carte générée au container
-  containerSkills.innerHTML += cardHTML;
+function nextSlide() {
+    const scrollAmount = devContainer.offsetWidth + parseInt(getComputedStyle(devContainer).gap);
+    devContainer.scrollBy({
+        left: scrollAmount,
+        behavior: 'smooth'
+    });
 }
+
+function previousSlide() {
+    const scrollAmount = devContainer.offsetWidth + parseInt(getComputedStyle(devContainer).gap);
+    devContainer.scrollBy({
+        left: -scrollAmount,
+        behavior: 'smooth'
+    });
+}
+
+
+
+// containerSkills = document.querySelector('.skills-container')
+
+// const skillsData = [
+//   {
+//       "id": 1,
+//       "title": "Créativité",
+//       "image_class": "crea",
+//       "image_name": "menu-ps",  // Nom de la première image
+//       "image_name2": "photoshopcolor", // Nom de la deuxième image
+//       "image_name3": "photoshop-toolBar",  // Nom de la troisième image
+//       "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae id optio quas, reprehenderit ipsum quos qui pariatur maiores sunt maxime.",
+//       "format" : "jpg"
+//   },
+//   {
+//       "id": 2,
+//       "title": "Développement Web",
+//       "image_class": "dev",
+//       "image_name": "HTML5",  // Nom de la première image
+//       "image_name2": "CSS3", // Nom de la deuxième image
+//       "image_name3": "Javascript",  // Nom de la troisième image
+//       "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac urna nisi. Proin at neque et dui ultricies tincidunt.",
+//       "format" :"svg"
+//   },
+//   {
+//       "id": 3,
+//       "title": "3D",
+//       "image_class": "unreal",
+//       "image_name": "ue5-tool",  // Nom de la première image
+//       "image_name2": "ue5-dock", // Nom de la deuxième image
+//       "image_name3": "ue5-file",  // Nom de la troisième image
+//       "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod justo vitae neque fringilla, ac pretium ligula condimentum.",
+//       "format" : "png"
+//   },
+//   {
+//       "id": 4,
+//       "title": "Montage vidéo",
+//       "image_class": "video",
+//       "image_name": "toolbar",  // Nom de la première image
+//       "image_name2": "timeline", // Nom de la deuxième image
+//       "image_name3": "effect",  // Nom de la troisième image
+//       "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet quos eveniet cupiditate tempora. Suscipit rerum commodi distinctio molestias.",
+//       "format" : "png"
+//   }
+// ];
+
+// const lengthData = skillsData.length; 
+// for (let i = 0; i < lengthData; i++) {
+//   const skill = skillsData[i];  // récupère l'élément actuel
+
+//   // Construction des chemins des images avec les noms dynamiques
+//   const imagePath1 = `img/${skill.image_name}.${skill.format}`;
+//   const imagePath2 = `img/${skill.image_name2}.${skill.format}`;
+//   const imagePath3 = `img/${skill.image_name3}.${skill.format}`;
+
+//   const cardHTML = `
+//       <div class="skills-card">
+//           <div class="skills-card-background ${skill.image_class}">
+//           <img src="${imagePath1}" alt="" class="${skill.image_name}">
+//               <img src="${imagePath2}" alt="" class="${skill.image_name2}">
+//               <img src="${imagePath3}" alt="" class="${skill.image_name3}">
+//           </div>
+//           <div class="skills-card-text">
+//               ${skill.text}
+//           </div>
+              
+//       </div>
+//   `;
+
+//   // Ajoute la carte générée au container
+//   containerSkills.innerHTML += cardHTML;
+// }
